a902cef1fa92b83f0c779af89f76c347b3db94fa
saravanan <saravanan.9344308178@gmail.com>
2024-07-22 12:19:19 +0530

    day 1 12

--- src/day1/infix_to_PostFix.java
+++ src/day1/infix_to_PostFix.java
@@ -6,11 +6,28 @@ public class infix_to_PostFix {
     public static void main(String[] args) {
         String a = "(p+q)*(m-n)";
         Stack<Character> stack = new Stack<>();
+        StringBuilder res = new StringBuilder();
         for(Character ch: a.toCharArray()){
-            if(ch == '('){
+            if(Character.isLetter(ch)){
+                res.append(ch);
+            }
+            else if(ch == '('){
                 stack.push(ch);
             } else if (ch == ')') {
+                    while (!stack.isEmpty() && stack.peek() != '('){
+                        res.append(stack.pop());
+                    }
+                    stack.pop()
+            }else{
+                switch (ch){
+                    case '+':
+                    case '-':
+                        while (stack.peek() == '+' || stack.peek() == '-' || ){
+                            res.append(stack.pop());
+                        }
+                    case '%':
 
+                }
             }
         }
     }
